a138ed30584fb2e3636340ea8059a930a1979f52
Anderson Reyes <andersonr@spotify.com>
2024-10-30 22:26:01 -0400

    project 1 done

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,9 @@
 const std = @import("std");
-const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+const in_memory_store = @import("in_memory_store.zig");
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+test {
+    _ = in_memory_store;
+    std.testing.refAllDecls(@This());
+    // or refAllDeclsRecursive
 }
--- src/storage/mod.rs
+++
@@ -1,8 +0,0 @@
-mod engine;
-mod in_memory_basic;
-mod log_structured;
-mod result;
-
-pub use engine::Engine;
-pub use in_memory_basic::InMemoryStorage;
-pub use log_structured::LogStructured;
