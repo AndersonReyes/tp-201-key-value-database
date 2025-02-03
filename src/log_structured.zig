6e48870cf7a4a11bc4291d0b50eac0735bd65f8e
Anderson Reyes <reyesanderson428@gmail.com>
2025-02-03 17:27:33 -0500

    got log writing working

--- src/log_structured.zig
+++ src/log_structured.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 const in_memory_store = @import("in_memory_store.zig");
 
 const LogStructuredStore = struct {
-    inner_store: in_memory_store.InMemoryStore,
     data_dir: std.fs.Dir,
 
     const Self = @This();
@@ -10,15 +9,14 @@ const LogStructuredStore = struct {
     const LogEntry = struct { key: []const u8, value: ?[]const u8 = null, op: []const u8 };
     const max_row_size: usize = 1024; // 1mb row size (key + value)
 
-    pub fn init(data_dir: std.fs.Dir, allocator: std.mem.Allocator) LogStructuredStore {
+    pub fn init(data_dir: std.fs.Dir) LogStructuredStore {
         return LogStructuredStore{
-            .inner_store = in_memory_store.InMemoryStore.init(allocator),
             .data_dir = data_dir,
         };
     }
 
     /// remove key from the store. Returns boolean if key exist and it was removed
-    pub fn remove(self: *Self, key: []const u8) !bool {
+    pub fn remove(self: *Self, key: []const u8) !void {
         var log = try self.data_dir.createFile("log.ndjson", .{ .truncate = false, .exclusive = false });
         defer log.close();
         try log.seekFromEnd(0);
@@ -26,8 +24,6 @@ const LogStructuredStore = struct {
         // TODO: does not check if they key actually exist in the log
         try std.json.stringify(.{ .key = key, .op = "remove" }, .{}, log.writer());
         _ = try log.write("\n");
-
-        return self.inner_store.remove(key);
     }
 
     /// put a key in the store
@@ -38,8 +34,6 @@ const LogStructuredStore = struct {
 
         try std.json.stringify(.{ .key = key, .value = value, .op = "set" }, .{}, log.writer());
         _ = try log.write("\n");
-
-        try self.inner_store.set(key, value);
     }
 
     /// retrieve a key from the store
@@ -70,74 +64,93 @@ const LogStructuredStore = struct {
             }
         }
 
-        // std.debug.print("key = {s}, value = {s}, op = {s}\n", .{ log_entry.key, log_entry.value, log_entry.op });
         return value;
     }
 
     /// destroy
-    pub fn deinit(self: *Self) void {
-        self.data_dir.close();
-        self.inner_store.deinit();
-    }
+    pub fn deinit(_: *Self) void {}
 };
 
-test "remove should return false when empty" {
+test "remove should true when removing a real value" {
     var tmp = std.testing.tmpDir(.{});
     defer tmp.cleanup();
-    var store = LogStructuredStore.init(tmp.dir, std.testing.allocator);
-
-    try std.testing.expectEqual(store.remove("invalid"), false);
-}
-
-test "remove should true when removing a real value" {
-    // var tmp = std.testing.tmpDir(.{});
-    // defer tmp.cleanup();
-    const dir = try std.fs.cwd().openDir("test-data", .{});
-    var store = LogStructuredStore.init(dir, std.testing.allocator);
+    var store = LogStructuredStore.init(tmp.dir);
     defer store.deinit();
 
-    try store.inner_store.set("1", "123456");
-    try std.testing.expectEqual(true, store.remove("1"));
-    // calling again should not remove anything
-    try std.testing.expectEqual(false, store.remove("1"));
+    var log = try tmp.dir.createFile("log.ndjson", .{ .truncate = false, .read = true, .exclusive = false });
+    defer log.close();
+
+    try log.seekTo(0);
+    try store.remove("2");
+
+    try log.seekTo(0);
+    var buf_reader = std.io.bufferedReader(log.reader());
+    const reader = buf_reader.reader();
+    var buf: [1024]u8 = undefined;
+    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        try std.testing.expectEqualStrings("{\"key\":\"2\",\"op\":\"remove\"}", line);
+    }
+    // else try std.testing.expect(false);
 }
 
 test "set should store the value" {
-    // var tmp = std.testing.tmpDir(.{});
-    // defer tmp.cleanup();
-    const dir = try std.fs.cwd().openDir("test-data", .{});
-    var store = LogStructuredStore.init(dir, std.testing.allocator);
+    var tmp = std.testing.tmpDir(.{});
+    defer tmp.cleanup();
+    var store = LogStructuredStore.init(tmp.dir);
     defer store.deinit();
 
-    try store.set("1", "123456");
-    try std.testing.expectEqual(1, store.inner_store.storage.count());
-    try std.testing.expectEqual("123456", store.inner_store.get("1"));
+    var log = try tmp.dir.createFile("log.ndjson", .{ .truncate = false, .exclusive = false });
+    defer log.close();
 
-    try store.set("1", "123");
-    try std.testing.expectEqual("123", store.inner_store.get("1"));
-    try std.testing.expectEqual(1, store.inner_store.storage.count());
+    try log.seekFromEnd(0);
 
-    try store.set("2", "12322");
-    try std.testing.expectEqual("12322", store.inner_store.get("2"));
-    try std.testing.expectEqual(2, store.inner_store.storage.count());
+    const writer = log.writer();
+
+    try std.json.stringify(.{ .key = "2", .value = "123456", .op = "set" }, .{}, writer);
+    _ = try log.write("\n");
+
+    const actual1 = (try store.get("2")).?;
+    try std.testing.expectEqualStrings("123456", actual1);
+
+    try std.json.stringify(.{ .key = "1", .value = "one", .op = "set" }, .{}, writer);
+    _ = try log.write("\n");
+
+    const actual2 = (try store.get("1")).?;
+    try std.testing.expectEqualStrings("one", actual2);
 }
 
 test "get should retrieve the value at key" {
     var tmp = std.testing.tmpDir(.{});
     defer tmp.cleanup();
-    // const dir = try std.fs.cwd().openDir("test-data", .{});
-    var store = LogStructuredStore.init(tmp.dir, std.testing.allocator);
+    var store = LogStructuredStore.init(tmp.dir);
     defer store.deinit();
 
-    try store.inner_store.set("1", "123456");
-    try std.testing.expectEqual("123456", store.get("1"));
+    var log = try tmp.dir.createFile("log.ndjson", .{ .truncate = true, .exclusive = false });
+    defer log.close();
+
+    try log.seekFromEnd(0);
+
+    try std.json.stringify(.{ .key = "2", .value = "1234", .op = "set" }, .{}, log.writer());
+    _ = try log.write("\n");
+
+    const actual = (try store.get("2")).?;
+    try std.testing.expectEqualStrings("1234", actual);
 }
 
 test "get value that does not exist should return null" {
     var tmp = std.testing.tmpDir(.{});
     defer tmp.cleanup();
-    var store = LogStructuredStore.init(tmp.dir, std.testing.allocator);
+    var store = LogStructuredStore.init(tmp.dir);
     defer store.deinit();
 
+    var log = try tmp.dir.createFile("log.ndjson", .{ .truncate = true, .exclusive = false });
+    defer log.close();
+
+    try log.seekFromEnd(0);
+
+    try std.json.stringify(.{ .key = "2", .value = "1234", .op = "set" }, .{}, log.writer());
+    _ = try log.write("\n");
+
     try std.testing.expectEqual(null, store.get("1"));
+    try std.testing.expectEqual(null, store.get("12"));
 }
